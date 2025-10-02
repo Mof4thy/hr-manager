dda520898f978f71b110f0d0efe6bf51aa9ffd18
fathy <2021170827@cis.asu.edu.eg>
2025-10-02 14:52:07 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <>
       <LanguageProvider>
         <FormProvider>
-          <Router basename="/hr-manager">
+          <Router>
             <Routes>
               <Route path='/applicant-form' element={<ApplicantForm />} />
               <Route path='/login' element={<Login />} />
